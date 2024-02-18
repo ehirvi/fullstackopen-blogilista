11e373db8fa3bf96e732c4d63d41bdee0379eb0d
ehirvi <eino.hirvi@gmail.com>
2024-02-18 15:07:35 +0200

    4.5 apufunktioita ja yksikkötestejä, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,7 +3,18 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
-  return blogs.reduce(((total, blog) => total + blog.likes), 0)
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((mostLiked, blog) => blog.likes > mostLiked.likes ? blog : mostLiked, blogs[0])
+  return blogs.length === 0
+    ? null
+    : {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes
+    }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
