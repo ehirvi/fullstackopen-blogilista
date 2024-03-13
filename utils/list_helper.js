857d1b22b0bddf67339cc9866c87108edc972be7
ehirvi <eino.hirvi@gmail.com>
2024-03-13 21:55:49 +0200

    fix

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -37,21 +37,17 @@ const mostLikes = (blogs) => {
     return null
   }
   const authors = []
-  const likes = []
   const output = []
 
   for (let blog of blogs) {
     if (authors.includes(blog.author)) {
-      likes[authors.indexOf(blog.author)] += blog.likes
       output[authors.indexOf(blog.author)][1] += blog.likes
     } else {
       authors.push(blog.author)
-      likes.push(blog.likes)
       output.push([blog.author, blog.likes])
     }
   }
   const outputSorted = _.orderBy(output, [1], 'desc')
-  console.log(outputSorted)
   return {
     author: outputSorted[0][0],
     likes: outputSorted[0][1]
