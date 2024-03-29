772cfbeb320cfb1dca687d8d984f75bc3400c862
ehirvi <eino.hirvi@gmail.com>
2024-03-29 15:19:14 +0200

    4.19 blogilistan laajennus, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,33 +1,45 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 const helper = require('../tests/test_helper')
 
+const getTokenFrom = (req) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.get('/', async (req, res) => {
-  const allBlogs = await Blog.find({}).populate('user', {blogs: 0})
+  const allBlogs = await Blog.find({}).populate('user', { blogs: 0 })
   res.json(allBlogs)
 })
 
 blogsRouter.post('/', async (req, res, next) => {
   const body = req.body
 
-  // let user = await User.findById(body.userId)
-  const users = await User.find({})
-  const user = users[0]
-
-  const newBlog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-    user: user._id
-  })
-
   try {
+    const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET)
+    if (!decodedToken.id) {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+
+    const newBlog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
+
     const savedBlog = await newBlog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
     res.status(201).json(savedBlog)
+
   } catch (e) {
     next(e)
   }
--- utils/middleware.js
+++ utils/middleware.js
@@ -16,11 +16,13 @@ const errorHandler = (err, req, res, next) => {
   logger.error(err.message)
 
   if (err.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
+    return res.status(400).json({ error: 'malformatted id' })
   } else if (err.name === 'ValidationError') {
-    return res.status(400).send({ error: err.message })
+    return res.status(400).json({ error: err.message })
   } else if (err.name === 'MongoServerError' && err.message.includes('E11000 duplicate key error')) {
-    return res.status(400).send({ error: 'expected \'username\' to be unique' })
+    return res.status(400).json({ error: 'expected \'username\' to be unique' })
+  } else if (err.name === "JsonWebTokenError") {
+    return res.status(400).json({error: 'token missing or invalid'})
   }
 
   next(err)
